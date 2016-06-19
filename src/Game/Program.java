711c1ab2b55408ede57e5c4ef11602ab0f219a13
belyanka <belyankamew@gmail.com>
2016-06-19 22:25:25 +0300

    Frame update changed. Multi-balls editors.

--- src/Game/Program.java
+++ src/Game/Program.java
@@ -7,14 +7,16 @@ import java.util.concurrent.TimeUnit;
 public class Program {
 	final static int ROW = 10;
 	final static int COL = 10;
-	final static int BALL_NUMBER = 1;
+	final static int BALL_NUMBER = 2;
 
 
 	public static void main(String[] args) {
 		//инициализируем поле, м€ч и интерфейс
 		ArrayList<Position> start = new ArrayList();
 		start.add(new Position(6,2));
+		start.add(new Position(6,3));
 		ArrayList<Position> finish = new ArrayList();
+		finish.add(new Position(2,6));
 		finish.add(new Position(2,6));
 
 		Level level = new Level(ROW, COL, BALL_NUMBER, start, finish);
